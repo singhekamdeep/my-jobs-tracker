cda14c88396e9159d06523904cfffd94e6f5886b
Ekam <ekamdeeps12@gmail.com>
2026-06-26 00:52:32 +0530

    Fix Vercel 404s and remove extension auto-open

--- extension/src/background/service-worker.ts
+++ extension/src/background/service-worker.ts
@@ -1,9 +1,4 @@
 // Background service worker
 
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === 'install') {
-    chrome.tabs.create({
-      url: 'https://my-jobs-tracker.vercel.app/login?installed=true',
-    });
-  }
-});
+// Background service worker
+
